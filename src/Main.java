9c1b6a69797cceca720acebf575eabd97364e40e
gizemsacal <46420726+gizemsacal@users.noreply.github.com>
2021-04-07 10:06:16 +0300

    Methods example2

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String mesaj= "Bugün hava çok güzel.";
+      String yeniMesaj= sehırVer();
+        System.out.println(yeniMesaj);
+        int sayi = topla(15,7);
+        System.out.println(sayi);
+
+           //metot=operasyon=fonksiyon hepsi aynı şeydir.
+    }
+    public static void ekle(){
+        System.out.println("Eklendi.");
+    }
+    public static void sil(){
+        System.out.println("Silindi.");
+    }
+    public static void guncelle(){
+        System.out.println("Güncellendi.");
+    }
+    public static int topla (int sayi1,int sayi2){
+        return sayi1 + sayi2;
+    }
+    public static String sehırVer(){
+        return "Ankara";
+}
+}
